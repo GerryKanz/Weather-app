9bb1c8de6c1c67ecd9cdce14691356d86448f405
gerry <kanzarag@gmail.com>
2025-01-18 21:35:32 +0900

    added https route to fetch urls

--- app/api.tsx
+++ app/api.tsx
@@ -1,7 +1,7 @@
 export default function data() {
     const api = {
         key: process.env.NEXT_PUBLIC_WEATHER_API_KEY,
-        baseUrl: 'http://api.openweathermap.org/data/2.5/',
+        baseUrl: 'https://api.openweathermap.org/data/2.5/',
         weatherMapsUrl: 'https://tile.openweathermap.org/map/'
     }
     return api
