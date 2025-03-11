5cba3148b5c8a377a1c8fc3d5b0438c42d268fa5
gerry <kanzarag@gmail.com>
2025-03-11 15:28:16 +0900

    1.1.14 Beta

--- app/ui/moreCardDetail.tsx
+++ app/ui/moreCardDetail.tsx
@@ -2,44 +2,40 @@
 import { Weather } from "./interfaces"
 import { WiHumidity, WiUmbrella, WiThermometer, WiStrongWind, WiSnowflakeCold } from "react-icons/wi";
 
-export default function MoreCardDetail({ weather, main, wind, rain, snow }: Weather) {
-    console.log(weather)
-    // const tempMin = props.main?.temp_min;
-    console.log(rain)
-    console.log()
+export default function MoreCardDetail({ main, wind, rain, snow }: Weather) {
 
-    return <div className="flex flex-row justify-evenly border-t font-manrope
-    border-slate-700 pt-4 text-black mt-5">
+    return <div className="flex flex-row justify-evenly border-t border-slate-700 pt-4 font-manrope text-black text-center mt-5 text-sm md:text-base
+">
 
-        <div className="border-r-2 border-slate-700 pr-5">
+        <div className="border-r border-slate-700 pr-2">
             <p className="font-bold text-3xl text-slate-400"><WiHumidity /></p>
             <p className="mt-2">{main.humidity} %</p>
         </div>
-        <div className="border-r-2 border-slate-700 pr-5">
+        <div className="border-r border-slate-700 pr-2">
             <p className="font-bold text-3xl text-slate-400"><WiThermometer /></p>
             <p className="mt-2">{main.pressure} hPa</p>
         </div>
-        <div className="border-r-2 border-slate-700 pr-5">
+        <div className="border-r border-slate-700 pr-2">
             <p className="font-bold text-3xl text-slate-400"><WiStrongWind /></p>
             <p className="mt-2">{wind.speed} Mps</p>
         </div>
 
         {rain ?
-            <div className="border-r-2 border-slate-700 pr-5">
+            <div className="border-r border-slate-700 pr-2">
                 <p className="font-bold text-3xl text-slate-400"><WiUmbrella /></p>
                 <p className="mt-2">{rain['3h']} mm</p>
             </div>
-            : <div className="border-r-2 border-slate-700 pr-5">
+            : <div className="border-r border-slate-700 pr-2">
                 <p className="font-bold text-3xl text-slate-400"><WiUmbrella /></p>
                 <p className="mt-2">0.00 mm</p>
             </div>}
 
         {snow ?
-            <div className="border-r-2 border-slate-700 pr-5">
+            <div className="border-r border-slate-700 pr-2">
                 <p className="font-bold text-3xl text-slate-400"><WiSnowflakeCold /></p>
                 <p className="mt-2">{snow['3h']} mm</p>
             </div>
-            : <div className="border-r-2 border-slate-700 pr-5">
+            : <div className="border-r border-slate-700 pr-2">
                 <p className="font-bold text-3xl text-slate-400"><WiSnowflakeCold /></p>
                 <p className="mt-2">0.00 mm</p>
             </div>}
